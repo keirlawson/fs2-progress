fbfa731074149bb56bb8a2ae8c438de2a99caa21
Keir Lawson <keirlawson@gmail.com>
2022-04-14 22:16:31 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,13 @@
+ThisBuild / scalaVersion     := "2.13.8"
+ThisBuild / version          := "0.1.0-SNAPSHOT"
+ThisBuild / organization     := "io.github.keirlawson"
+ThisBuild / organizationName := "keirlawson"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "fs2-progress",
+    libraryDependencies ++= Seq(
+      "me.tongfei" % "progressbar" % "0.9.3",
+      "co.fs2" %% "fs2-core" % "3.2.7"
+    )
+  )
---
+++ src/main/scala/fs2/progress/ProgressBar.scala
@@ -0,0 +1,38 @@
+package example
+
+import cats.effect.IO
+import me.tongfei.progressbar.{ProgressBar => pBar}
+import fs2.{Stream, Pipe}
+import cats.implicits._
+import cats.effect.kernel.Resource
+import cats.effect.kernel.Sync
+
+trait ProgressBar[F[_]] {
+
+  def stepTo(n: Long): F[Unit]
+
+  def pipe[A]: Pipe[F, A, A]
+  
+}
+
+object ProgressBar {
+  def resource[F[_] : Sync](name: String, initialMax: Long): Resource[F, ProgressBar[F]] = Resource.fromAutoCloseable(Sync[F].delay(new pBar(name, initialMax))).map { pb =>
+    new ProgressBar[F] {
+
+      override def pipe[A]: Pipe[F,A,A] = { input =>
+        input.zipWithIndex.evalTap { case (_, idx) =>
+          stepTo(idx)
+        }.map(_._1)
+      }
+
+
+      override def stepTo(n: Long): F[Unit] = Sync[F].delay(pb.stepTo(n))
+
+    }
+  }
+
+  def stream[F[_] : Sync](name: String, initialMax: Long): Stream[F, ProgressBar[F]] = {
+    Stream.resource(resource[F](name, initialMax))
+  }
+
+}
