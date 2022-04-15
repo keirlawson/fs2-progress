fc1d3388b8558c67008a52f40b8c5d1682f50b92
Keir Lawson <keirlawson@gmail.com>
2022-04-15 16:14:20 +0100

    Specify scaladoc

--- src/main/scala/fs2/progress/ProgressBar.scala
+++ src/main/scala/fs2/progress/ProgressBar.scala
@@ -10,30 +10,71 @@ import me.tongfei.progressbar.ProgressBarBuilder
 
 trait ProgressBar[F[_]] {
 
+  /**
+   * Pauses this current progress.
+   */
   def pause: F[Unit]
 
+  /**
+   * Prompts the progress bar to refresh. Normally a user should not call this function.
+   */
   def refresh: F[Unit]
 
+  /** Resets the progress bar to its initial state (where progress equals to 0). */
   def reset: F[Unit]
 
+  /**
+   * Resumes this current progress.
+   */
   def resume: F[Unit]
 
+  /**
+   * Sets the extra message at the end of the progress bar.
+   * @param msg New message
+   */
   def setExtraMessage(msg: String): F[Unit]
 
+  /**
+   * Returns the current progress.
+   */
   def current: F[Long]
 
+  /**
+   * Returns the extra message at the end of the progress bar.
+   */
   def extraMessage: F[String]
 
+  /**
+   * Returns the maximum value of this progress bar.
+   */
   def max: F[Long]
 
+  /**
+   * Returns the name of this task.
+   */
   def taskName: F[String]
 
+  /**
+   * Gives a hint to the maximum value of the progress bar.
+   * @param n Hint of the maximum value. A value of -1 indicates that the progress bar is indefinite.
+   */
   def maxHint(n: Long): F[Unit]
 
+  /**
+   * Advances this progress bar by one step.
+   */
   def step: F[Unit]
 
+  /**
+   * Advances this progress bar by a specific amount.
+   * @param n Step size
+   */
   def stepBy(n: Long): F[Unit]
 
+  /**
+   * Advances this progress bar to the specific progress value.
+   * @param n New progress value
+   */
   def stepTo(n: Long): F[Unit]
 
   def pipe[A]: Pipe[F, A, A]
@@ -41,10 +82,19 @@ trait ProgressBar[F[_]] {
 }
 
 object ProgressBar {
+  /**
+   * Creates a Resource yielding a progress bar with the specific taskName name and initial maximum value.
+   * @param task Task name
+   * @param initialMax Initial maximum value
+   */
   def resource[F[_] : Sync](name: String, initialMax: Long): Resource[F, ProgressBar[F]] = Resource.eval(Sync[F].delay(
     new ProgressBarBuilder().setTaskName(name).setInitialMax(initialMax)
   )).flatMap(resource(_))
 
+  /**
+   * Creates a Resource yielding a progress bar reflecting the supplied builder.
+   * @param builder ProgressBarBuilder representing the desired configuration
+   */
   def resource[F[_] : Sync](builder: ProgressBarBuilder): Resource[F, ProgressBar[F]] = Resource.fromAutoCloseable(Sync[F].delay(builder.build())).map { pb =>
     new ProgressBar[F] {
 
@@ -84,6 +134,11 @@ object ProgressBar {
     }
   }
 
+  /**
+   * Creates a Stream yielding a progress bar with the specific taskName name and initial maximum value.
+   * @param task Task name
+   * @param initialMax Initial maximum value
+   */
   def stream[F[_] : Sync](name: String, initialMax: Long): Stream[F, ProgressBar[F]] = {
     Stream.resource(resource[F](name, initialMax))
   }
